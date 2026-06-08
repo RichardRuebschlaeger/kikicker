b63465c79192f90de9f8e8c75da5badc8242becf
richard <awvr777@freenet.de>
2026-06-08 18:57:23 +0200

    Added some ball detection functionality. Guaranteed to be broken.

--- ball_detector.py
+++ ball_detector.py
@@ -9,7 +9,7 @@ import cv2
 import numpy as np
 from output import output_position
 
-def detect_ball(rawframe_bgr, calibrate):
+def detect_ball(rawframe_bgr, calibration):
     """
     
 
@@ -17,24 +17,46 @@ def detect_ball(rawframe_bgr, calibrate):
     ----------
     rawframe_bgr : TYPE
         The image from which the ball position is to be determined.
-    calibrate : TYPE
+        NOTE: Operating under the assumption that there is only one ball (orange object).
+    calibration : TYPE
         Calibration refers to the (re-) calculation of the projection matrix used to level the playing field image.
-        Set to true to (re-) calculate; set to false to use a previously calculated one (if it exists) or set matrix to be used.
+        Set to true to (re-) calculate; set to false to use a previously calculated one (if it exists); or set to matrix to be used instead.
+
 
     Returns
     -------
     None.
+    
+    
+    Important Links
+    ---------------
+    https://docs.opencv.org/4.13.0/da/d6e/tutorial_py_geometric_transformations.html
+    https://learnopencv.com/find-center-of-blob-centroid-using-opencv-cpp-python/
 
     """
-    global cameraCalibrationMatrix
-    if calibrate:
-        #TODO: Calculate projection matrix:
-        cameraCalibrationMatrix = None #Set to real value later
-    selectionMask = cv2.inRange(cv2.cvtColor(rawframe_bgr,cv2.COLOR_BGR2HSV),np.array([10,120,129]),np.array([40,255,255]))
+    if type(calibration)==bool:
+        if calibration:
+            TODO
+        else:
+            global cameraCalibrationMatrix
+    #elif type()==:#TODO
+    #    cameraCalibrationMatrix = calibration
+    
+    # Display raw frame and the selection mask from the raw frame:
+    rawSelectionMask = cv2.inRange(cv2.cvtColor(rawframe_bgr,cv2.COLOR_BGR2HSV),np.array([10,120,129]),np.array([40,255,255]))
     cv2.imshow("raw frame", rawframe_bgr)
-    cv2.imshow("raw frame selection", selectionMask)
+    cv2.imshow("raw frame selection", rawSelectionMask)
     
     # TODO project and crop image
-    # TODO get ball position from projected image
-    # TODO send ball position via output function
-    #output_position(x, y)
+    projectedFrame_bgr = cv2.warpPerspective(rawframe_bgr, cameraCalibrationMatrix, NEW SIZE);
+    cv2.imshow("projected frame", projectedFrame_bgr)
+    
+    # Get ball position from projected image:
+    projectedSelectionMask = cv2.inRange(cv2.cvtColor(projectedFrame_bgr,cv2.COLOR_BGR2HSV),np.array([10,120,129]),np.array([40,255,255]))
+    cv2.imshow("projected frame selection", projectedSelectionMask)
+    M = cv2.moments(projectedSelectionMask)
+    x = int(M["m10"] / M["m00"])
+    y = int(M["m01"] / M["m00"])
+    
+    # Send ball position via output function
+    output_position(x, y)
